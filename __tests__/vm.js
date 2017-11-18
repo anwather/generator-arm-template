1c8d08452fc5cbd9771002ba03d70e2690464c0f
Brian Farnhill <brfarn@microsoft.com>
2017-11-18 15:21:34 +1100

    Added vm object

---
+++ __tests__/vm.js
@@ -0,0 +1,42 @@
+'use strict';
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+var generator;
+
+describe('generator-arm-template:vm', () => {
+  beforeAll(() => {
+    generator = helpers.createGenerator(
+      'arm-template:vm',
+      [path.join(__dirname, '../generators/vm')],
+      null,
+      null
+    );
+  });
+
+  it('contains a storage account', () => {
+    var template = {
+      $schema:
+        'https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#',
+      contentVersion: '1.0.0.0',
+      parameters: {},
+      variables: {},
+      resources: [],
+      outputs: {}
+    };
+    template = generator.addResource(template, {
+      name: 'testName',
+      location: 'testLocation',
+      username: 'admin',
+      password: 'password',
+      publisher: 'publisher',
+      offer: 'offer',
+      sku: 'sku',
+      version: 'latest',
+      storageAccount: 'storageAccountTest',
+      nic: 'nicTest'
+    });
+    assert.equal(template.resources[0].type, 'Microsoft.Compute/virtualMachines');
+  });
+});
--- generators/vm/index.js
+++ generators/vm/index.js
@@ -24,14 +24,14 @@ module.exports = class extends Generator {
         chalk.blue('   AAAAAAAAAA                 AAAAAAAAAAAAAAAA    \n') +
         chalk.blue('                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
         chalk.blue('               AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
-        '\n\nWelcome to the Azure ARM Template project generator for Network interfaces!\n'
+        '\n\nWelcome to the Azure ARM Template project generator for virtual machines!\n'
     );
 
     const prompts = [
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of the network interface?'
+        message: 'What is the name of the VM?'
       },
       {
         type: 'input',
@@ -41,26 +41,52 @@ module.exports = class extends Generator {
       },
       {
         type: 'input',
-        name: 'publicIpName',
-        message:
-          'What is the name of the public IP address resource to associate to this NIC? (blank for none)'
+        name: 'size',
+        message: 'What size should the VM be?'
       },
       {
         type: 'input',
-        name: 'networkName',
-        message:
-          'What is the name of the virtual network address to assoicate to this NIC?'
+        name: 'username',
+        message: 'What is the username of the local admin account?'
       },
       {
         type: 'input',
-        name: 'subnetName',
-        message:
-          'What is the name of the subnet in the virtual network to put this NIC in to?'
+        name: 'password',
+        message: 'What is the password of the local admin account?'
       },
       {
         type: 'input',
-        name: 'privateIpAddress',
-        message: 'What private IP address should the NIC use? (blank for dynamic)'
+        name: 'publisher',
+        message: 'Who is the publisher of the image for the VM?',
+        default: 'MicrosoftWindowsServer'
+      },
+      {
+        type: 'input',
+        name: 'offer',
+        message: 'What VM image offer should be used?',
+        default: 'WindowsServer'
+      },
+      {
+        type: 'input',
+        name: 'sku',
+        message: 'What image SKU should be used?',
+        default: '2016-Datacenter'
+      },
+      {
+        type: 'input',
+        name: 'version',
+        message: 'What image version should be used?',
+        default: 'latest'
+      },
+      {
+        type: 'input',
+        name: 'storageAccount',
+        message: 'What storage account should be used for the default disk?'
+      },
+      {
+        type: 'input',
+        name: 'nic',
+        message: 'What is the name of the NIC that should be used?'
       }
     ];
 
@@ -73,45 +99,63 @@ module.exports = class extends Generator {
   writing() {
     var templatePath = this.destinationPath('azuredeploy.json');
     var template = this.fs.readJSON(templatePath);
-    template = template.addResource(template, this.props);
+    template = this.addResource(template, this.props);
     this.fs.writeJSON(templatePath, template, null, 2);
   }
 
   addResource(template, properties) {
     template.resources.push({
       apiVersion: '2015-05-01-preview',
-      type: 'Microsoft.Network/networkInterfaces',
+      type: 'Microsoft.Compute/virtualMachines',
       name: properties.name,
       location: properties.location,
       dependsOn: [],
       properties: {
-        ipConfigurations: [
-          {
-            name: 'ipconfig1',
-            properties: {
-              privateIPAllocationMethod: 'Static',
-              privateIPAddress: '192.168.0.4',
-              publicIPAddress: {
-                id:
-                  "[resourceId('Microsoft.Network/publicIPAddresses', '" +
-                  properties.publicIpName +
-                  "')]"
-              },
-              subnet: {
-                id:
-                  "[concat(resourceId('Microsoft.Network/virtualNetworks', '" +
-                  properties.networkName +
-                  "'), '/subnets/" +
-                  properties.subnetName +
-                  "')]"
-              }
+        hardwareProfile: {
+          vmSize: properties.size
+        },
+        osProfile: {
+          computerName: properties.name,
+          adminUsername: properties.username,
+          adminPassword: properties.password,
+          windowsConfiguration: {
+            provisionVMAgent: true,
+            enableAutomaticUpdates: true
+          }
+        },
+        storageProfile: {
+          imageReference: {
+            publisher: properties.publisher,
+            offer: properties.offer,
+            sku: properties.sku,
+            version: properties.version
+          },
+          osDisk: {
+            name: properties.name + '-disk',
+            caching: 'ReadWrite',
+            createOption: 'FromImage',
+            vhd: {
+              uri:
+                'https://' +
+                properties.storageAccount +
+                '.blob.core.windows.net/vhds/' +
+                properties.name +
+                '-disk.vhd'
             }
           }
-        ]
+        },
+        networkProfile: {
+          networkInterfaces: [
+            {
+              id:
+                "[resourceId('Microsoft.Network/networkInterfaces','" +
+                properties.nic +
+                "')]"
+            }
+          ]
+        }
       }
     });
-    // TODO: Swap private IP in and set static/dynamic for allocation method
-    // TODO: remove the public IP address if there is none allocated
     return template;
   }
 
