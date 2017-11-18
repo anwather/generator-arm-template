d1a74525a9971769619c8600b834ba1dec2ef8cb
Brian Farnhill <brfarn@microsoft.com>
2017-11-18 15:46:31 +1100

    Added DSC extension support

---
+++ __tests__/vm-dscextension.js
@@ -0,0 +1,40 @@
+'use strict';
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+var generator;
+
+describe('generator-arm-template:vm-dscextension', () => {
+  beforeAll(() => {
+    generator = helpers.createGenerator(
+      'arm-template:vm-dscextension',
+      [path.join(__dirname, '../generators/vm-dscextension')],
+      null,
+      null
+    );
+  });
+
+  it('contains a storage account', () => {
+    var template = {
+      $schema:
+        'https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#',
+      contentVersion: '1.0.0.0',
+      parameters: {},
+      variables: {},
+      resources: [],
+      outputs: {}
+    };
+    template = generator.addResource(template, {
+      vmName: 'testName',
+      location: 'testLocation',
+      modulesUrl: 'test',
+      configurationFunction: 'test',
+      sasToken: 'test'
+    });
+    assert.equal(
+      template.resources[0].type,
+      'Microsoft.Compute/virtualMachines/extensions'
+    );
+  });
+});
---
+++ generators/vm-dscextension/index.js
@@ -0,0 +1,102 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      chalk.blue('                          A                       \n') +
+        chalk.blue('                       AAAA                       \n') +
+        chalk.blue('                     AAAAAA AAA                   \n') +
+        chalk.blue('                  AAAAAAA  AAAAA                  \n') +
+        chalk.blue('                AAAAAAAA  AAAAAAA                 \n') +
+        chalk.blue('              AAAAAAAAA  AAAAAAAAA                \n') +
+        chalk.blue('            AAAAAAAAAA  AAAAAAAAAAA               \n') +
+        chalk.blue('           AAAAAAAAAA  AAAAAAAAAAAAAA             \n') +
+        chalk.blue('          AAAAAAAAAA  AAAAAAAAAAAAAAAA            \n') +
+        chalk.blue('         AAAAAAAAAA   AAAAAAAAAAAAAAAAA           \n') +
+        chalk.blue('        AAAAAAAAAA      AAAAAAAAAAAAAAAA          \n') +
+        chalk.blue('      AAAAAAAAAAA         AAAAAAAAAAAAAAA         \n') +
+        chalk.blue('     AAAAAAAAAAA            AAAAAAAAAAAAAA        \n') +
+        chalk.blue('    AAAAAAAAAAA               AAAAAAAAAAAAAA      \n') +
+        chalk.blue('   AAAAAAAAAAA                 AAAAAAAAAAAAAA     \n') +
+        chalk.blue('   AAAAAAAAAA                 AAAAAAAAAAAAAAAA    \n') +
+        chalk.blue('                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
+        chalk.blue('               AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
+        '\n\nWelcome to the Azure ARM Template project generator for DSC VM Extension!\n'
+    );
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'vmName',
+        message: 'What is the name of the VM?'
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where should the resource be created?',
+        default: '[resourceGroup().location]'
+      },
+      {
+        type: 'input',
+        name: 'modulesUrl',
+        message: 'What is the URL of the configuration zip file?'
+      },
+      {
+        type: 'input',
+        name: 'configurationFunction',
+        message: 'What script/function should be called for the configuration?'
+      },
+      {
+        type: 'input',
+        name: 'sasToken',
+        message: 'What SAS token is needed to access the configuration zip file'
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    var templatePath = this.destinationPath('azuredeploy.json');
+    var template = this.fs.readJSON(templatePath);
+    template = this.addResource(template, this.props);
+    this.fs.writeJSON(templatePath, template, null, 2);
+  }
+
+  addResource(template, properties) {
+    template.resources.push({
+      type: 'Microsoft.Compute/virtualMachines/extensions',
+      name: properties.vmName + '/Microsoft.Powershell.DSC',
+      apiVersion: '2015-05-01-preview',
+      location: '[resourceGroup().location]',
+      dependsOn: [
+        "[resourceId('Microsoft.Compute/virtualMachines', '" + properties.vmName + "')]"
+      ],
+      properties: {
+        publisher: 'Microsoft.Powershell',
+        type: 'DSC',
+        typeHandlerVersion: '2.*',
+        settings: {
+          ModulesUrl: properties.modulesUrl,
+          ConfigurationFunction: properties.configurationFunction,
+          SasToken: properties.sasToken,
+          Properties: {}
+        },
+        protectedSettings: {
+          Items: {}
+        }
+      }
+    });
+    return template;
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
